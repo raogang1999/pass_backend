ac28cfc5b21a9848d4aa05335254fe1b6af1f4f2
raogang1999 <raogang1999@163.com>
2022-05-26 21:49:55 +0800

    initialize

---
+++ src/main/java/top/iprivacy/pass_backend/PassBackendApplication.java
@@ -0,0 +1,13 @@
+package top.iprivacy.pass_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PassBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/iprivacy/pass_backend/PassBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package top.iprivacy.pass_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
